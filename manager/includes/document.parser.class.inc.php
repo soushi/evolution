fa3b3020848334f5b9ac14848d962e5b910867a6
yama <yamamoto@kyms.ne.jp>
2012-04-24 10:15:09 +0900

    Code format $modx->mergePlaceholderContent() (speed up)

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -784,7 +784,7 @@ class DocumentParser {
             for ($i= 0; $i < $cnt; $i++) {
                 $v= '';
                 $key= $matches[1][$i];
-                if (is_array($this->placeholders) && array_key_exists($key, $this->placeholders))
+                if (is_array($this->placeholders) && isset($this->placeholders[$key]))
                     $v= $this->placeholders[$key];
                 if ($v === '')
                     unset ($matches[0][$i]); // here we'll leave empty placeholders for last.
