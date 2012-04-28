7e94c76b857033fb900a3f8eb5f32cc3cf126721
yama <yamamoto@kyms.ne.jp>
2012-04-28 23:00:03 +0900

    Delete API $modx->getUserData()

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -2442,12 +2442,6 @@ class DocumentParser {
 		return $this->parseChunk($src, $ph, $left, $right, $mode);
 	}
 	
-	function getUserData()
-	{
-        include_once($this->config["base_path"] . "manager/includes/extenders/getUserData.extender.php");
-        return $tmpArray;
-    }
-
 	function toDateFormat($timestamp = 0, $mode = '')
 	{
         $timestamp = trim($timestamp);
